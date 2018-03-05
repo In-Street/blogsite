e4e137edfc8924219cd5aa350f33c8e40bce4077
“Cheng <873401580@qq.com>
2018-03-05 22:32:57 +0800

    去除无效信息

--- blog-api/src/main/java/cyf/blog/api/controller/IndexController.java
+++ blog-api/src/main/java/cyf/blog/api/controller/IndexController.java
@@ -1,8 +1,13 @@
 package cyf.blog.api.controller;
 
+import cyf.blog.base.Constants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 首页
@@ -21,5 +26,23 @@ public class IndexController  {
         return "themes/default/index";
     }
 
+    /**
+     * 首页分页
+     *
+     * @param request request
+     * @param p       第几页
+     * @param limit   每页大小
+     * @return 主页
+     */
+    @GetMapping(value = "page/{p}")
+    public String index(HttpServletRequest request, @PathVariable int p, @RequestParam(value = "limit", defaultValue = "12") int limit) {
 
+        p = p < 0 || p > Constants.MAX_PAGE ? 1 : p;
+        PageInfo<ContentVo> articles = contentService.getContents(p, limit);
+        request.setAttribute("articles", articles);
+        if (p > 1) {
+            this.title(request, "第" + p + "页");
+        }
+        return this.render("index");
+    }
 }
