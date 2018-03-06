07192c055c6eb7054b500b98ab9ef6543b51cee4
chengyufei <chengyufei@daishumovie.com>
2018-03-06 18:56:03 +0800

    首页开工及dao生成

---
+++ blog-dao/src/main/java/cyf/blog/dao/mapper/ContentsMapper.java
@@ -0,0 +1,40 @@
+package cyf.blog.dao.mapper;
+
+import cyf.blog.dao.model.Contents;
+import cyf.blog.dao.model.ContentsExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ContentsMapper {
+    long countByExample(ContentsExample example);
+
+    int deleteByExample(ContentsExample example);
+
+    int deleteByPrimaryKey(Integer cid);
+
+    int insert(Contents record);
+
+    int insertSelective(Contents record);
+
+    int selfPlusMinus(@Param("columnName") String columnName, @Param("operator") String operator, @Param("count") int count, @Param("example") Object example);
+
+    int selfPlusMinusByPrimaryKey(@Param("columnName") String columnName, @Param("operator") String operator, @Param("count") int count, @Param("id") int id);
+
+    List<Contents> selectByExampleWithBLOBs(ContentsExample example);
+
+    List<Contents> selectByExample(ContentsExample example);
+
+    Contents selectByPrimaryKey(Integer cid);
+
+    int updateByExampleSelective(@Param("record") Contents record, @Param("example") ContentsExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") Contents record, @Param("example") ContentsExample example);
+
+    int updateByExample(@Param("record") Contents record, @Param("example") ContentsExample example);
+
+    int updateByPrimaryKeySelective(Contents record);
+
+    int updateByPrimaryKeyWithBLOBs(Contents record);
+
+    int updateByPrimaryKey(Contents record);
+}
